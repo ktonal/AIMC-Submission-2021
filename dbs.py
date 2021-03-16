ebddd6ecf5c6e5fe74c699736ec1de84aa9762f8
Antoine Daurat <antoinedaurat@gmail.com>
2021-03-16 18:36:21 +0100

    add SampleRNN notebook and dbs.py

---
+++ dbs.py
@@ -0,0 +1,8 @@
+
+
+DBS = {
+    "throat": ["Gutural.m4a", "Throat Singing.mp3"],
+    "yodel": ["Yodel_female.wav", "Yodel_male.wav", "Yodel_male_Duet.wav"],
+    "pansori": ["pansori_Neptune_data_male.mp3", "Pansori_Bum_female.mp3"],
+
+}
